e5af7ec72b1df049005fb2aaf22cb82bc0cc79c9
Rohit Negi <utopian.studios.dev@gmail.com>
2026-03-14 12:40:42 +0530

    Implement Post hog

---
+++ app/posthog-pageview.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import { usePostHog } from 'posthog-js/react'
+
+export default function PostHogPageView() {
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+    const posthog = usePostHog()
+
+    useEffect(() => {
+        if (pathname && posthog) {
+            let url = window.location.origin + pathname
+            const search = searchParams.toString()
+            if (search) {
+                url += `?${search}`
+            }
+            posthog.capture('$pageview', { '$current_url': url })
+        }
+    }, [pathname, searchParams, posthog])
+
+    return null
+}
---
+++ app/providers.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
+import { useEffect, Suspense } from 'react'
+import PostHogPageView from './posthog-pageview'
+
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+            api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+            person_profiles: 'identified_only',
+            capture_pageview: false,
+        })
+    }, [])
+
+    return (
+        <PHProvider client={posthog}>
+            <Suspense fallback={null}>
+                <PostHogPageView />
+            </Suspense>
+            {children}
+        </PHProvider>
+    )
+}
